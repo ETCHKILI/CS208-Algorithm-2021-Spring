ecf921e03e508ebddfa3b48ea626eb3e73fe0581
Etch <guoyubinetch@gmail.com>
2021-03-16 13:44:18 +0800

    lab 00 01 upgraded

---
+++ Lab_00/walk_stairs.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+int trace(int n);
+
+int main()
+{
+    int t,n,l;
+    std::cin >> t;
+    int * pInt;
+
+    for (int i = 0; i < t; ++i)
+    {
+        std::cin >> n >> l;
+        pInt = new int [n];
+
+
+
+    }
+}
+
+int trace(int n)
+{
+    if (n != 1)
+    {
+
+    }
+
+
+}
---
+++ Lab_01/stable_match.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i) {
+
+    }
+
+
+}
+
+class boy{
+public:
+};
+
